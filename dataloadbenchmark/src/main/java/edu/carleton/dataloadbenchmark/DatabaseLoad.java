d08d03bfa94e1ad0419549ed31496e8c25c42ffa
rowleyn <rowleyn@carleton.edu>
2020-02-24 15:23:13 -0600

    Add skeleton of MongoDB benchmark implementation

--- dataloadbenchmark/src/main/java/edu/carleton/dataloadbenchmark/DatabaseLoad.java
+++ dataloadbenchmark/src/main/java/edu/carleton/dataloadbenchmark/DatabaseLoad.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 public interface DatabaseLoad {
+    // format data and then insert it into the database.
+    // returns true if the operation was successful, and false if not
     boolean insert(Map par, List<String[]> sem, List<String[]> set, List<List<Map>> sparse, List<Map> dense);
+
+    // returns a string that identifies the database and schema
     String name();
 }
---
+++ dataloadbenchmark/src/main/java/edu/carleton/dataloadbenchmark/MongoDBBenchmark.java
@@ -0,0 +1,20 @@
+package edu.carleton.dataloadbenchmark;
+
+import java.util.List;
+import java.util.Map;
+
+public class MongoDBBenchmark implements DatabaseLoad {
+
+    public boolean insert(Map par, List<String[]> sem, List<String[]> set, List<List<Map>> sparse, List<Map> dense) {
+        // format data and insert into db
+        // return true if successful and false if not
+
+        return true;
+    }
+
+    public String name() {
+        // return a string that identifies this database and schema implementation
+
+        return "";
+    }
+}
