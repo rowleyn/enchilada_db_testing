3d9603dd65d6b2b530cca21e13c161367e4073a3
rowleyn <rowleyn@carleton.edu>
2020-02-28 15:46:04 -0600

    Remove old testing statements

--- dataloadbenchmark/src/main/java/edu/carleton/dataloadbenchmark/DataLoadBenchmark.java
+++ dataloadbenchmark/src/main/java/edu/carleton/dataloadbenchmark/DataLoadBenchmark.java
@@ -17,20 +17,6 @@ public class DataLoadBenchmark {
                 "data/20100812_metals_00005_masscal.cal",
                 "data/sizecal_20100701.noz");
 
-
-//        System.out.println(data.par);
-//        System.out.println(Arrays.toString(data.sem.get(0)));
-//        System.out.println(Arrays.toString(data.sem.get(5000)));
-//        System.out.println(Arrays.toString(data.set.get(0)));
-//        System.out.println(Arrays.toString(data.set.get(5000)));
-//        System.out.println(data.particlenames.get(0));
-//        System.out.println(data.particlenames.get(5000));
-//        System.out.println(data.sparsemaps.get(0));
-//        System.out.println(data.sparsemaps.get(5000));
-//        System.out.println(data.densemaps.get(0));
-//        System.out.println(data.densemaps.get(5000));
-
-
         // Initialize variables
         List<DatabaseLoad> dbs = new ArrayList<>();
         BufferedWriter writer = new BufferedWriter(new FileWriter("results.txt"));
