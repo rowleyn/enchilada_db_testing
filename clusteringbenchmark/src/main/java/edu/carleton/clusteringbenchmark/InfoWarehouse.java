d95fb51b6041932a3c0de8caec51955e3a961abc
rowleyn <rowleyn@carleton.edu>
2020-03-05 14:37:55 -0600

    Add descriptions of each method in InfoWarehouse so far

--- clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/InfoWarehouse.java
+++ clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/InfoWarehouse.java
@@ -6,46 +6,48 @@ import java.util.ArrayList;
 
 public interface InfoWarehouse {
 
-    public Collection getCollection(int collectionID);
+    public Collection getCollection(int collectionID); // simple, gets a collection (dataset) from db with associated metadata
 
     public int createEmptyCollection( String datatype,
                                       int parent,
                                       String name,
                                       String comment,
-                                      String description);
+                                      String description); // simple, creates a new empty collection in the db
 
-    public void atomBatchInit();
+    public void atomBatchInit(); // sets up a SQL Statement object for batch add/move
 
-    public boolean addAtomBatch(int atomID, int parentID);
+    public boolean addAtomBatch(int atomID, int parentID); // adds an atom creation to the batch Statement
 
-    public void bulkInsertAtom(int newChildID, int newHostID) throws Exception;
+    public boolean deleteAtomBatch(int atomID, Collection collection); // adds an atom deletion to the batch Statement
 
-    public void atomBatchExecute();
+    public boolean deleteAtomsBatch(String atomIDs, Collection collection); // adds multiple atom deletions from the batch Statement
 
-    public boolean deleteAtomsBatch(String atomIDs, Collection collection);
+    public void atomBatchExecute(); // executes the batch Statement
 
-    public boolean deleteAtomBatch(int atomID, Collection collection);
+    public void bulkInsertInit() throws Exception; // initalizes a bulk insert using temporary files
 
-    public void bulkInsertExecute() throws Exception;
+    public void bulkInsertAtom(int newChildID, int newHostID) throws Exception; // writes an atom to bulk-insert to the temp file
 
-    public Connection getCon(); // will need to change as this is SQL-specific
+    public void bulkInsertExecute() throws Exception; // executes a bulk insert, reading from temp files
 
-    public CollectionCursor getAtomInfoOnlyCursor(Collection collection);
+    public Connection getCon(); // gets a connection to the SQL db, will need to change as this is SQL-specific
 
-    public void bulkInsertInit() throws Exception;
+    public CollectionCursor getAtomInfoOnlyCursor(Collection collection); // simple, just returns a new AtomInfoOnlyCursor object on collection
 
-    public String getCollectionDatatype(int subCollectionNum);
+    public String getCollectionDatatype(int subCollectionNum); // simple, just gets the datatype (ATOFMS, AMs, etc.) of a collection
 
-    public ArrayList<ArrayList<String>> getColNamesAndTypes(String datatype, DynamicTable table);
+    public ArrayList<ArrayList<String>> getColNamesAndTypes(String datatype, DynamicTable table); // gets the names of columns for a given datatype, this is metadata
 
-    public int insertParticle(String dense, ArrayList<String> sparse,Collection collection, int nextID);
+    public int insertParticle(String dense, ArrayList<String> sparse,Collection collection, int nextID); // inserts a particle's sparse (peaks) and dense info into the db
 
-    public int getNextID();
+    public int getNextID(); // gets the max id of any particle, this is the next id I guess?
 
-    public boolean addCenterAtom(int centerAtomID, int centerCollID);
+    public boolean addCenterAtom(int centerAtomID, int centerCollID); // simple, adds a particle to the Centeratoms table
 
     public boolean setCollectionDescription(Collection collection,
-                                            String description);
+                                            String description); // simple, updates a collection's description
 
+    public void updateInternalAtomOrder(Collection collection); // this is some kind of relational bookkeeping method, not sure exactly what it's doing
 
+    public void updateAncestors(Collection collection); // same kind of thing, relational bookkeeping. Again not sure exactly what it does
 }
