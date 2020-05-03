29501b03bbfe6362842b0046566ca110717948c2
Kelsey Krantz <krantz77@gmail.com>
2020-05-03 14:31:39 -0400

    Started warehousetests

--- clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/WarehouseTests.java
+++ clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/WarehouseTests.java
@@ -1,4 +1,28 @@
 package edu.carleton.clusteringbenchmark;
 
+import edu.carleton.clusteringbenchmark.database.CassandraWarehouse;
+import edu.carleton.clusteringbenchmark.database.InfoWarehouse;
+
+import java.io.IOException;
+
+
 public class WarehouseTests {
+    private static InfoWarehouse db;
+    public static void main (String[]args) throws Exception {
+            db = new CassandraWarehouse();
+            bulkInsertTest();
+    }
+
+    public static void bulkInsertTest() throws Exception {
+
+        try {
+            db.bulkInsertInit();
+        } catch (IOException  e){
+            System.err.println("Error in bulk insert init");
+            e.printStackTrace();
+        }
+        //Need to get AtomID and ParentID
+        //db.bulkInsertAtom();
+    }
+
 }
