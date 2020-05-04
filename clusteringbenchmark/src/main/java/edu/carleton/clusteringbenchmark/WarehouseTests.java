9686f3ad63e4ba45665297b85c5c98a09d1a33ec
Kelsey Krantz <krantz77@gmail.com>
2020-05-03 22:40:17 -0400

    Test for create collection, get collection

--- clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/WarehouseTests.java
+++ clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/WarehouseTests.java
@@ -3,6 +3,8 @@ package edu.carleton.clusteringbenchmark;
 import edu.carleton.clusteringbenchmark.database.CassandraWarehouse;
 import edu.carleton.clusteringbenchmark.database.InfoWarehouse;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 
@@ -10,13 +12,19 @@ public class WarehouseTests {
     private static InfoWarehouse db;
     public static void main (String[]args) throws Exception {
             db = new CassandraWarehouse();
+            createCollectionTest();
             bulkInsertTest();
+
     }
-    public static int createCollectionTest() {
-        return -1;
+    public static void createCollectionTest() {
+        String dataType = db.getCollectionDatatype(0);
+        int collectionid = db.createEmptyCollection(dataType, 0, "name", "comment", "description");
+        System.out.println(db.getCollectionName(collectionid));
+        System.out.println(db.getCollection(collectionid));
     }
 
     public static void bulkInsertTest() throws Exception {
+        long start = System.currentTimeMillis();
 
         try {
             db.bulkInsertInit();
@@ -28,6 +36,12 @@ public class WarehouseTests {
         int atomID = db.getNextID();
         db.bulkInsertAtom(atomID, atomID +1 );
         db.bulkInsertExecute();
+        System.out.println("Success");
+
+        long end = System.currentTimeMillis();
+        System.out.print("Time for Bulk Insert");
+        long timeelapsed = end - start;
+        System.out.println(" time elapsed" + timeelapsed + " milliseconds");
 
     }
 
