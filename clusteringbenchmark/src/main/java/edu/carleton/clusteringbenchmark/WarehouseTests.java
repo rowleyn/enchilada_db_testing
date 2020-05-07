dee31685f4395a3e010891523c04cca7fb164679
rowleyn <rowleyn@carleton.edu>
2020-05-06 20:17:20 -0500

    Add insertParticle and cursor tests

--- clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/WarehouseTests.java
+++ clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/WarehouseTests.java
@@ -1,18 +1,41 @@
 package edu.carleton.clusteringbenchmark;
 
+import edu.carleton.clusteringbenchmark.ATOFMS.ParticleInfo;
+import edu.carleton.clusteringbenchmark.analysis.BinnedPeakList;
+import edu.carleton.clusteringbenchmark.atom.ATOFMSAtomFromDB;
 import edu.carleton.clusteringbenchmark.database.CassandraWarehouse;
+import edu.carleton.clusteringbenchmark.database.CollectionCursor;
 import edu.carleton.clusteringbenchmark.database.InfoWarehouse;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class WarehouseTests {
+
     private static InfoWarehouse db;
+    private static CollectionCursor cursor;
+
+    private static int NUM_PARTICLES = 5;
+
+    private static String filename = "'ThisFile'";
+    private static String dateString = "'1983-01-19 05:05:00.0'";
+    private static float laserPower = (float)0.01191983;
+    private static float size = (float)0.5;
+    private static int scatterDelay = 10;
+
+    private static int posPeakLocation1 = 19;
+    private static int negPeakLocation1 = -20;
+    private static int peak1Height = 80;
+    private static int posPeakLocation2 = 100;
+    private static int negPeakLocation2 = -101;
+    private static int peak2Height = 100;
+
     public static void main (String[]args) throws Exception {
             db = new CassandraWarehouse();
             createCollectionTest();
+            List<Integer> atomids = insertParticlesTest();
             bulkInsertTest();
 
     }
@@ -23,6 +46,27 @@ public class WarehouseTests {
         System.out.println(db.getCollection(collectionid));
         System.out.println(db.getCollectionSize(collectionid));
         //db.getColNamesAndTypes()
+    }
+
+    public static List<Integer> insertParticlesTest() {
+        ArrayList<String> sparseData = new ArrayList<>();
+        sparseData.add(posPeakLocation1 + ", " +  peak1Height + ", 0.1, " + peak1Height);
+        sparseData.add(negPeakLocation1 + ", " +  peak1Height + ", 0.1, " + peak1Height);
+        sparseData.add(posPeakLocation2 + ", " +  peak2Height + ", 0.1, " + peak2Height);
+        sparseData.add(negPeakLocation2 + ", " +  peak2Height + ", 0.1, " + peak2Height);
+
+        String denseData = dateString + "," + laserPower + "," + size + "," + scatterDelay + ", " + filename;
+
+        List<Integer> atomids = new ArrayList<>();
+
+        for (int i = 0; i < NUM_PARTICLES; i++) {
+            int nextID = db.getNextID();
+            int particleID = db.insertParticle(denseData, sparseData, db.getCollection(0), nextID);
+            assert nextID == particleID;
+            atomids.add(particleID);
+        }
+
+        return atomids;
     }
 
     public static void bulkInsertTest() throws Exception {
@@ -47,4 +91,24 @@ public class WarehouseTests {
 
     }
 
+    public static void cursorTest() throws Exception{
+        for (int i = 0; i < 2; i++) {
+            while (cursor.next()) {
+                ParticleInfo info = cursor.getCurrent();
+                ATOFMSAtomFromDB dense = info.getATOFMSParticleInfo();
+                BinnedPeakList sparse = info.getBinnedList();
+                assert filename.equals(dense.getFilename());
+                assert dateString.equals(dense.getDateString());
+                assert laserPower == dense.getLaserPower();
+                assert size == dense.getSize();
+                assert scatterDelay == dense.getScatDelay();
+                assert peak1Height == sparse.getAreaAt(posPeakLocation1);
+                assert peak1Height == sparse.getAreaAt(negPeakLocation1);
+                assert peak2Height == sparse.getAreaAt(posPeakLocation2);
+                assert peak2Height == sparse.getAreaAt(negPeakLocation2);
+            }
+            cursor.reset();
+        }
+        cursor.close();
+    }
 }
