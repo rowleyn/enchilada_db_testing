4db4afa6fe190bc4617c889180f9072fad293b6d
rowleyn <rowleyn@carleton.edu>
2020-05-07 17:59:25 -0500

    Add tests for addCenterAtom and getColNamesAndTypes

--- clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/WarehouseTests.java
+++ clusteringbenchmark/src/main/java/edu/carleton/clusteringbenchmark/WarehouseTests.java
@@ -5,6 +5,7 @@ import edu.carleton.clusteringbenchmark.analysis.BinnedPeakList;
 import edu.carleton.clusteringbenchmark.atom.ATOFMSAtomFromDB;
 import edu.carleton.clusteringbenchmark.database.CassandraWarehouse;
 import edu.carleton.clusteringbenchmark.database.CollectionCursor;
+import edu.carleton.clusteringbenchmark.database.DynamicTable;
 import edu.carleton.clusteringbenchmark.database.InfoWarehouse;
 
 import java.io.IOException;
@@ -96,6 +97,28 @@ public class WarehouseTests {
         long timeelapsed = end - start;
         System.out.println(" time elapsed" + timeelapsed + " milliseconds");
 
+    }
+
+    public static void centerAtomsTest() {
+        int collectionid = db.createEmptyCollection("ATOFMS", -1, "name", "comment", "description");
+        boolean result = db.addCenterAtom(0, collectionid);
+        assert result;
+        result = db.addCenterAtom(0, 100);
+        assert !result;
+    }
+
+    public static void colNamesTest() {
+        List<ArrayList<String>> dense = db.getColNamesAndTypes("ATOFMS", DynamicTable.AtomInfoDense);
+        List<ArrayList<String>> sparse = db.getColNamesAndTypes("ATOFMS", DynamicTable.AtomInfoSparse);
+        assert dense.get(0).get(0).equals("time") && dense.get(0).get(1).equals("DATETIME");
+        assert dense.get(1).get(0).equals("laserpower") && dense.get(1).get(1).equals("REAL");
+        assert dense.get(2).get(0).equals("size") && dense.get(2).get(1).equals("REAL");
+        assert dense.get(3).get(0).equals("scatdelay") && dense.get(3).get(1).equals("INT");
+        assert dense.get(4).get(0).equals("specname") && dense.get(4).get(1).equals("VARCHAR(8000)");
+        assert sparse.get(0).get(0).equals("masstocharge") && sparse.get(0).get(1).equals("REAL");
+        assert sparse.get(1).get(0).equals("area") && sparse.get(1).get(1).equals("INT");
+        assert sparse.get(2).get(0).equals("relarea") && sparse.get(2).get(1).equals("REAL");
+        assert sparse.get(3).get(0).equals("height") && sparse.get(3).get(1).equals("INT");
     }
 
     public static void cursorTest() {
